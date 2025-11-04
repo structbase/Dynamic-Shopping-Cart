fcaa3591a7a0d9e7209520ebd3698f7c81cd3376
Abenezer <abitysank@gmail.com>
2025-11-04 15:35:57 -0500

    A bit of inventory was done for possible options for preset for shopping name, pictures, and pricing.

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const totalPriceSpan = document.getElementById("total-price");
 
 let totalPrice = 0;
 
+let inventory = [
+    { name: "Apple", price: 1.29, image: "./assets/apple.png" },
+    { name: "Avocado", price: 1.99, image: "./assets/avocado.png" },
+    { name: "Broccoli", price: 2.49, image: "./assets/broccoli.png" },
+    { name: "Butter", price: 4.49, image: "./assets/butter.png" },
+    { name: "Carrot", price: 0.99, image: "./assets/carrot.png" },
+    { name: "Cheese", price: 5.49, image: "./assets/cheese.png" },
+    { name: "Egg", price: 0.39, image: "./assets/egg.png" },
+    { name: "Grape", price: 2.99, image: "./assets/grape.png" },
+    { name: "Milk", price: 3.79, image: "./assets/milk.png" },
+    { name: "Onion", price: 0.79, image: "./assets/onion.png" },
+    { name: "Orange", price: 1.19, image: "./assets/orange.png" },
+    { name: "Peach", price: 1.49, image: "./assets/peach.png" },
+    { name: "Pepper", price: 1.59, image: "./assets/pepper.png" },
+    { name: "Potato", price: 0.99, image: "./assets/potato.png" },
+    { name: "Strawberry", price: 3.49, image: "./assets/strawberry.png" },
+    { name: "Yogurt", price: 1.19, image: "./assets/yogurt.png" },
+];
+
 // Function to update the total price
 function updateTotalPrice(amount) {
     totalPrice += amount;
